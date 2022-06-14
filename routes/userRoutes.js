0a95116d2ccae583205af65d42d6f0b9de57625c
Muhammad-Nabeel-Asif <nabeel.asif362@gmail.com>
2022-06-14 12:40:43 +0500

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,8 +18,8 @@ const {
 } = require("../controllers/user");
 
 const router = express.Router();
-router.route("/signup").post(signup);
 
+router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 
